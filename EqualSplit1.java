e30ad901d970f6970264f12275eda54f4b4b0d36
Haripriya2313 <69977674+Haripriya2313@users.noreply.github.com>
2021-11-10 19:33:47 +0530

    Create EqualSplit1.java

---
+++ EqualSplit1.java
@@ -0,0 +1,10 @@
+package com.workattech.splitwise.models.split1;
+
+import com.workattech.splitwise.models.User1;
+
+public class EqualSplit1 extends Split1 {
+
+    public EqualSplit(User1 user) {
+        super(user);
+    }
+}
