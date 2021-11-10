e0f48e43511e2dd137285b81328a4817970dd744
Haripriya2313 <69977674+Haripriya2313@users.noreply.github.com>
2021-11-10 19:51:51 +0530

    Update EqualExpense1.java

--- EqualExpense1.java
+++ EqualExpense1.java
@@ -7,14 +7,14 @@ import com.workattech.splitwise.models.split.Split1;
 import java.util.List;
 
 public class EqualExpense1 extends Expense1 {
-    public EqualExpense(double amnt, User1 paidBy, List<Split> splits, ExpenseMetadata1 expenseMetadata) {
+    public EqualExpense1(double amnt, User1 paidBy, List<Split> splits, ExpenseMetadata1 expenseMetadata) {
         super(amnt, paidBy, splits, expenseMetadata);
     }
 
     @Override
     public boolean validate() {
         for (Split split : getSplits()) {
-            if (!(split instanceof EqualSplit)) {
+            if (!(split instanceof EqualSplit1)) {
                 return false;
             }
         }
